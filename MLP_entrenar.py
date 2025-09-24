40135151eab35b75d7bc50d4526bf3acfc499d5c
Jorrodlop <jorrod03@ucm.es>
2025-09-24 17:32:37 +0200

    Eliminados datasets antiguos

--- MLP_entrenar.py
+++ MLP_entrenar.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 import torch
 import torch.nn.functional as F
 from MLP import MLP
@@ -7,11 +12,11 @@ from Funciones_relevantes import clasificacion
 
 ## MODELO a entrenar ##
 
-nombre_archivo_red = r"redes_disponibles\mlp_prueba_desc.json"  # Archivo donde se guarda la red
+nombre_archivo_red = r""  # Archivo donde se guarda la red
 
 
 ## HIPERPARAMETROS de entrenamiento ##
-stp_n = 5                  # Número de pasos de entrenamiento
+stp_n = 500                # Número de pasos de entrenamiento
 stp_sz = 0.001             # Tamaño del paso (learning rate)
 batch_sz = None            # Tamaño del batch (por defecto, todo el dataset)
 
@@ -28,8 +33,8 @@ sustituir_desc = False             # CUIDADO, ELIMINA LA DESCRIPCIÓN ANTERIOR
 añadir_info_mejora = True          # Añade informacion de como ha mejorado/empeorado el modelo sobre el test dado
 
 ## DATOS de test y entrenamiento ##
-archivo_entrenamiento = "datasets/nba_pergame_24_full.csv"
-archivo_test = "datasets/nba_pergame_24_full.csv"
+archivo_entrenamiento = ""
+archivo_test = ""
 xs_train, ys_train, etiquetas_train, xs_test, ys_test, etiquetas_test = procesar_datos(archivo_set_train=archivo_entrenamiento,
                                                                                     archivo_set_test=archivo_test,
                                                                                     modo_autoencoder=False,
