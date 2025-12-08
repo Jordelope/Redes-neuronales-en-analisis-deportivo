02a9c6547a5c028ade178eb361510c8fd08a5ff2
Jorrodlop <jorrod03@ucm.es>
2025-12-08 19:28:11 +0100

    Separado generar, guardar y visualizar clusters en func distintas.

--- Clustering.py
+++ Clustering.py
@@ -2,13 +2,12 @@
 Clustering.py
 -------------
 
-Este script proporciona dos funciones principales para agrupar (clusterizar) jugadores de la NBA y visualizar los resultados:
+Este script proporciona funciones para generar, guardar, cargar y visualizar clusters(grupos) de  jugadores de la NBA  a partir de embeddings generados por un autoencoder:
 
-1. clusterizar_pca: Procesa los datos originales, aplica reducción de dimensionalidad con PCA y agrupa los jugadores usando K-Means. La visualización se realiza en el espacio reducido por PCA.
-2. clustering_autoencoder: Agrupa directamente sobre los embeddings generados por un autoencoder, aplicando K-Means sobre ellos y visualizando los resultados en el espacio latente de un segundo autoencoder (encoder).
+1. generar_clusters: Usando el algoritmo de k-medias agrupa embeddings segun su proximidad en el espacio latente.
+2. guardar_clusters y cargar_clusters: Permite guardar y cargar los diccionarios con los clusters usando ficheros de tipo csv.
+3. visual_clusters: Usando un modelo de reduccion de dimensionalidad permite visualizar los embeddings en 2 o 3 dimensiones agrupando por colores segun unos clusters dados.
 
-La diferencia principal es que la primera función trabaja sobre los datos procesados originales y usa PCA para la reducción de dimensión, mientras que la segunda trabaja sobre embeddings aprendidos y utiliza el encoder de un autoencoder para la visualización.
-Ambas funciones permiten guardar los resultados de los clusters y generan gráficos con etiquetas de los jugadores.
 """
 import torch
 import pandas as pd
@@ -22,6 +21,74 @@ from Embeddings import cargar_embeddings
 from Procesar_datos_AE import procesar_datos
 
 
+
+
+def generar_clusters(embeddings_dict:dict, n_clusters:int):
+    # Preparar datos
+    jugadores = list(embeddings_dict.keys())
+    X = torch.tensor([embeddings_dict[j] for j in jugadores], dtype=torch.float32)
+
+    # K-means sobre embeddings originales
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
+    clusters = kmeans.fit_predict(X.numpy())
+    
+    # ---- Guardar resultados en diccionario ---
+    dicc_clusters = {jugador: int(cluster) for jugador, cluster in zip(jugadores, clusters)}
+    return dicc_clusters
+
+def guardar_clusters(dicc_clusters:dict, archivo_salida:str):
+    # --- Guardar como csv --
+    df_out = pd.DataFrame(list(dicc_clusters.items()), columns=["Player", "Cluster"])
+    df_out.to_csv(archivo_salida, index=False)
+    print(f"Clusters guardados en {archivo_salida}")
+
+def cargar_clusters(archivo_clusters:str):
+    df = pd.read_csv(archivo_clusters)
+    dicc_clusters = {}
+    for _,row in df.iterrows():
+        jugador = row['Player']
+        clt = int(row['Cluster'])
+        dicc_clusters[jugador] =clt
+    return dicc_clusters
+
+def visual_clusters(embeddings_dict:dict, dicc_clusters:dict, n_clusters:int, modelo_visual:Autoencoder=None):
+    
+    # Extraer encoder del modelo
+    modelo= cargar_modelo(modelo_visual)
+    encoder = modelo.encoder
+
+    # Preparar datos de embeddings
+    jugadores = list(embeddings_dict.keys())
+    clusters = list(dicc_clusters.values())
+    
+    # Transformamos embeddings a tensores para operar  
+    X = torch.tensor([embeddings_dict[j] for j in jugadores], dtype=torch.float32)
+    
+    # Reducir dimensión con encoder para graficar
+    with torch.no_grad():
+        X_encoded = encoder(X).numpy()  # Se asume que encoder devuelve 2D
+    
+    # Gráfico
+    plt.figure(figsize=(10, 8))
+    scatter = plt.scatter(X_encoded[:, 0], X_encoded[:, 1], c=clusters, cmap="tab10", alpha=0.7)
+    
+    # Etiquetas de todos los jugadores
+    for i, jugador in enumerate(jugadores):
+        plt.text(X_encoded[i, 0] + 0.02, X_encoded[i, 1] + 0.02, jugador, fontsize=7)
+
+    plt.title(f"Clusters sobre embeddings, graficados con encoder. (k={n_clusters})")
+    plt.xlabel("Dim 1")
+    plt.ylabel("Dim 2")
+    plt.colorbar(scatter, label="Cluster")
+    plt.tight_layout()
+    plt.show()
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+"""
+Las siguientes funciones se encuentran desactualizadas y están pendientes de ser eliminadas.
+"""
 
 def clusterizar_pca(csv_file, tipo_stats="completo", n_clusters=5, n_components=2):
     """
@@ -56,7 +123,6 @@ def clusterizar_pca(csv_file, tipo_stats="completo", n_clusters=5, n_components=
     plt.colorbar(scatter, label="Cluster")
     plt.tight_layout()
     plt.show()
-
 
 def clustering_autoencoder(archivo_modelo, embeddings_path, archivo_salida, n_clusters=5):
     """
@@ -109,4 +175,3 @@ def clustering_autoencoder(archivo_modelo, embeddings_path, archivo_salida, n_cl
     plt.colorbar(scatter, label="Cluster")
     plt.tight_layout()
     plt.show()
-
